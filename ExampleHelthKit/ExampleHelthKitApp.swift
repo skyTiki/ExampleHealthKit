6b48341aaedbde327ff11d16f997f37788760415
takatoshi.ichige <tichige1213@gmail.com>
2021-07-29 15:38:11 +0900

    Initial Commit

---
+++ ExampleHelthKit/ExampleHelthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleHelthKitApp.swift
+//  ExampleHelthKit
+//
+//  Created by S-wayMock2 on 2021/07/29.
+//
+
+import SwiftUI
+
+@main
+struct ExampleHelthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
